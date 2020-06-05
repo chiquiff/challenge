41093eab9c03cedee56fd4f95c612d4229f687ca
Fede Falour <federico.falour@rappi.com>
2020-06-05 10:38:30 -0300

    fixing exceptions and gitignore

--- microservices/employees/app.py
+++ microservices/employees/app.py
@@ -3,18 +3,24 @@ import pymysql.cursors
 from flask import jsonify
 import json
 import collections
+import errno
+from socket import error as socket_error
 
 app=Flask(__name__)
 
 @app.route("/employees", methods=["GET"])
 def test():
-    connection = pymysql.connect(host='mysql-container',
-                                 port=3306,
-                                 user='root',
-                                 password='secret',
-                                 db='employees',
-                                 charset='utf8mb4',
-                                 cursorclass=pymysql.cursors.DictCursor)
+    try:
+        connection = pymysql.connect(host='mysql-container',
+                                     port=3306,
+                                     user='root',
+                                     password='secret',
+                                     db='employees',
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+    except pymysql.err.OperationalError as e:
+        return json.dumps({'*** WAIT!! ***': 'PLEASE WAIT UNTIL THE DB FINISHES POPULATING...'})
+
     try:
         with connection.cursor() as cursor:
             sql = "select * from employees where gender = 'M' and birth_date = '1965-02-01' and hire_date > '1990-01-01' order by first_name ASC, last_name ASC;"
@@ -34,7 +40,7 @@ def test():
                 j = json.dumps(objects_list)
             return j
     except Exception as e:
-        return {'error': str(e)}
+        return json.dumps({'Unexpected error': str(e)})
     finally:
         connection.close()
 
